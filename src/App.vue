3e7d25d23a3b8a02cb4a9ef356748470b62f6c93
HsiaoHungWang <wang@ispan.com.tw>
2024-02-05 14:14:35 +0800

    加上 Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-import First from './components/First.vue';
-import HelloWorld from './components/HelloWorld.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <RouterLink to="/">首頁</RouterLink> |
-  <RouterLink to="/about">關於我們</RouterLink> |
-  <RouterLink to="/contact">聯絡我們</RouterLink> |
-  <hr />
-  <h2>Vue Project</h2>
-  <First></First>
-  <hello-world></hello-world>
-  <hr />
-  <RouterView></RouterView>
+  <div class="container">
+    <Navbar></Navbar>
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Vue Project</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <RouterLink to="/" class="nav-link active">首頁</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/about">關於我們</RouterLink>
+
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/contact">聯絡我們</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+    
+<script setup>
+
+</script>
+    
+<style></style>
