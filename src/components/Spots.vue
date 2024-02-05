555cc7e43389b677b9c5a8e46fb37d909ad5be2e
HsiaoHungWang <wang@ispan.com.tw>
2024-02-05 14:33:49 +0800

    環境設定檔 .env 的使用

--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -5,7 +5,10 @@
 <script setup>
 import { onMounted } from 'vue';
 const loadCategories = async () => {
-    const API_URL = "https://localhost:7259/api/Categories";
+    // console.log(import.meta.env.VITE_API_SPOTURL); //https://localhost:7259/api
+
+    // const API_URL = "https://localhost:7259/api/Categories";
+    const API_URL = `${import.meta.env.VITE_API_SPOTURL}/categories`;
     const response = await fetch(API_URL);
     const datas = await response.json();
     console.log(datas);
