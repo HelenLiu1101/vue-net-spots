864b8e95915cf695c301edcf3a3a25c5609d2ba8
HsiaoHungWang <wang@ispan.com.tw>
2024-02-05 14:26:57 +0800

    透過 fetch() 串接 API

--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -3,6 +3,19 @@
 </template>
     
 <script setup>
+import { onMounted } from 'vue';
+const loadCategories = async () => {
+    const API_URL = "https://localhost:7259/api/Categories";
+    const response = await fetch(API_URL);
+    const datas = await response.json();
+    console.log(datas);
+}
+
+onMounted(() => {
+    loadCategories();
+})
+
+
 
 </script>
     
