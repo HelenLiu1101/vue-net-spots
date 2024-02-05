7b9e95ea9ebb257a93081044f8bd70b1b6cccba5
HsiaoHungWang <wang@ispan.com.tw>
2024-02-05 12:12:16 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import First from './components/First.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+</script>
+
+<template>
+  <h2>Vue Project</h2>
+  <First></First>
+  <hello-world></hello-world>
+  <Home></Home>
+  <About></About>
+  <Contact></Contact>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+    <input type="text" v-model="msg" />
+    <h3>Hello,{{ msg }}</h3>
+</template>
+<script setup>
+import { ref } from 'vue'
+const msg = ref("")
+</script>
+<style scoped>
+h3 {
+    color: green;
+}
+</style>
