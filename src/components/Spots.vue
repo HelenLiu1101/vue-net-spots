92d49b35f73ed297c00005bcd59bf1858baa1fde
HsiaoHungWang <wang@ispan.com.tw>
2024-02-05 15:04:09 +0800

    傳遞JSON的搜尋條件給API

--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -3,19 +3,58 @@
 </template>
     
 <script setup>
-import { onMounted } from 'vue';
-const loadCategories = async () => {
-    // console.log(import.meta.env.VITE_API_SPOTURL); //https://localhost:7259/api
+import { onMounted, reactive } from 'vue';
+//ref
+//reactive
+//搜尋、分頁、排序的條件
+const terms = reactive({
+    "keyword": "北投",
+    "categoryId": 0,
+    "sortBy": "spotId",
+    "sortType": "asc",
+    "page": 1,
+    "pageSize": 9
+});
 
-    // const API_URL = "https://localhost:7259/api/Categories";
-    const API_URL = `${import.meta.env.VITE_API_SPOTURL}/categories`;
-    const response = await fetch(API_URL);
+//回傳的資料
+const result = reactive({
+    "totalPages": 1,
+    "spotsResult": []
+});
+
+//呼叫API
+const loadSpots = async () => {
+    const API_URL = `${import.meta.env.VITE_API_SPOTURL}/spots`;
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(terms),
+        headers: { 'Content-Type': 'application/json' }
+    });
     const datas = await response.json();
     console.log(datas);
+
 }
 
+
+
+
+
+
+
+
+// const loadCategories = async () => {
+//     // console.log(import.meta.env.VITE_API_SPOTURL); //https://localhost:7259/api
+
+//     // const API_URL = "https://localhost:7259/api/Categories";
+//     const API_URL = `${import.meta.env.VITE_API_SPOTURL}/categories`;
+//     const response = await fetch(API_URL);
+//     const datas = await response.json();
+//     console.log(datas);
+// }
+
 onMounted(() => {
-    loadCategories();
+    // loadCategories();
+    loadSpots();
 })
 
 
